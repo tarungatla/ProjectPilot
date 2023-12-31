523bd75b948214bac23f617dc4d475dac1389fe7
tarungatla <tarungatla75@gmail.com>
2023-12-31 17:58:41 +0530

    feat: organization info and board list ui added

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,21 @@
-import { auth } from "@clerk/nextjs";
+import { Suspense } from "react";
 
-const OrganizationPage = () => {
-    const { userId, orgId } = auth();
+import { Separator } from "@/components/ui/separator";
+
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+
+const OrganizationIdPage = async () => {
+
   return (
-      <div>OrganizationPage {userId} {orgId}</div>
-  )
-}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+          <BoardList />
+      </div>
+    </div>
+  );
+};
 
-export default OrganizationPage
+export default OrganizationIdPage;
