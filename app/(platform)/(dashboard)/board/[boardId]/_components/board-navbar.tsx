cbd653ad19807061c572ff90a9e0dbc14aba4c86
tarungatla <tarungatla75@gmail.com>
2024-01-05 15:02:26 +0530

    feat: Delete board completed

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,17 +1,18 @@
 import { Board } from "@prisma/client";
 
 import { BoardTitleForm } from "./board-title-form";
+import { BoardOptions } from "./board-options";
 
-interface BoardNavbarProps {
-  data: Board;
-};
 
 export const BoardNavbar = async ({
   data
-}: BoardNavbarProps) => {
+}: {data:Board}) => {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
-      <BoardTitleForm data={data} />  {/*To change the title from the navbar*/} 
+      <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
