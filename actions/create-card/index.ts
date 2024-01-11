49cb5ebbe9860cb2ccc245225eb6f3f2399e8096
tarungatla <tarungatla75@gmail.com>
2024-01-11 20:08:28 +0530

    feat: Audit logs db and route created

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -8,6 +8,8 @@ import { createSafeAction } from "@/lib/create-safe-action";
 
 import { CreateCard } from "./schema";
 import { InputType, ReturnType } from "./types";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { createAuditLog } from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -53,6 +55,12 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
+    await createAuditLog({
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      entityTitle: card.title,
+      action: ACTION.CREATE,
+    });
   } catch (error) {
     return {
       error: "Failed to create."
